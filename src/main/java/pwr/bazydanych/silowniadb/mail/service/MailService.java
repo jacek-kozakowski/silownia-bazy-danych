42b088634cc84b8b6b1283a3312722215f258974
Jacek Kozakowski <jk.kozakowski@gmail.com>
2025-11-18 20:30:50 +0100

    feat: Dodano MailService
    - Dodano konfiguracje mail w application.properties
    - Dodano konfiguracje mail i serwis
    - Dodano konfiguracje bazy danych w application.properties

--- src/main/java/pwr/bazydanych/silowniadb/mail/service/MailService.java
+++ src/main/java/pwr/bazydanych/silowniadb/mail/service/MailService.java
@@ -1,4 +1,26 @@
 package pwr.bazydanych.silowniadb.mail.service;
 
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class MailService {
+    private final JavaMailSender mailSender;
+
+    public void sendVerificationEmail(String to, String subject, String text) throws MessagingException {
+        MimeMessage msg = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(msg, true);
+
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setText(text, true);
+
+        mailSender.send(msg);
+    }
 }
